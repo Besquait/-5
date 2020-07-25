479496a321aad197d763f7aac89fc92cea0903a4
Besquait <58892425+Besquait@users.noreply.github.com>
2020-07-25 16:34:24 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".slider").slick({
+        prevArrow:' <button class="slick-prev" type="button" class="slick-prev"><img src="img/svg/left.svg"></button',
+        nextArrow:' <button class="slick-next" type="button" class="slick-next"><img src="img/svg/right.svg"></button',
+        autoplay: 3000,
+        dots:true,
+    })
+})
